0722fbd8d8fa2bb574bc07ffe01b3dcf5dd200a7
Adityam <k.adityam2117@gmail.com>
2026-07-10 22:06:21 +0530

    fix: update backend imports for deployment

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -2,14 +2,14 @@ import traceback
 
 from fastapi import APIRouter
 
-from backend.app.schemas import PredictionRequest
-from backend.app.predictor import predict_medical_plan
+from app.schemas import PredictionRequest
+from app.predictor import predict_medical_plan
 
 from fastapi import HTTPException
 
-from backend.app.config import MODEL_NAME, MODEL_VERSION
+from app.config import MODEL_NAME, MODEL_VERSION
 
-from backend.app.database import log_prediction
+from app.database import log_prediction
 
 router = APIRouter()
 
